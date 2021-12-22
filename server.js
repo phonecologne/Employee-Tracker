1bc7c959062bc800a9630ae9d88f368fe1d19903
Greg <aquamirror@gmail.com>
2021-12-22 01:31:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -77,3 +77,27 @@ function firstPrompt() {
     });
 }
 
+//now we want to show all employees in the system
+function viewEmployee() {
+  console.log("Viewing Employees\n");
+
+  var query = `Select e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+  ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+  ON m.id  = e.manager_id`
+
+  connection.query(query, function (er, res) {
+    if (err) throw err;
+
+    console.table(res);
+    console.log("Employees Viewed\n");
+
+    firstPrompt();
+  });
+
+}
+
