3e551adfaa0fa57ab1455e8b3a849407afaa48a0
Greg <aquamirror@gmail.com>
2021-12-25 04:41:27 -0800

    Update seed.sql

--- SQL/seed.sql
+++ SQL/seed.sql
@@ -8,3 +8,15 @@ INSERT INTO deparment (name)
 VALUES ("Finance");
 INSERT INTO deparment (name)
 VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+
